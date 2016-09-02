ae60e78522b261d50072c7b5586f8f19a1c3536e
spuhpointer <madars.vitolins@gmail.com>
2016-09-03 01:08:43 +0300

    Fixes

--- src/build-chart.sh
+++ src/build-chart.sh
@@ -9,7 +9,7 @@ export NDRX_BENCH_FILE=`pwd`/result.txt
 #
 # Generate the chart
 #
-export NDRX_BENCH_TITLE="Service call (one server calls one client)"
+export NDRX_BENCH_TITLE="Service call (one client calls one server)"
 export NDRX_BENCH_X_LABEL="Msg Size (bytes)"
 export NDRX_BENCH_Y_LABEL="Calls Per Second (tpcall()/sec)"
 export NDRX_BENCH_OUTFILE="result.png"
